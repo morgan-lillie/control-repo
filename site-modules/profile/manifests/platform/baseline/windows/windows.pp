10fdd57decb089fe2c5ea048f50c27f7ad0bd170
Morgan Lillie <61731997+morgan-lillie@users.noreply.github.com>
2020-11-13 14:55:42 +0000

    Update windows.pp
    Added cont group

--- site-modules/profile/manifests/platform/baseline/windows/windows.pp
+++ site-modules/profile/manifests/platform/baseline/windows/windows.pp
@@ -3,6 +3,10 @@ class profile::platform::baseline::users::windows {
   user { 'Cont User':
     ensure => present,
     password => 'puppet',
-    groups => ['Administrators'],
+    groups => ['Administrators', 'Cont Group'],
     }
+   
+  group { 'Cont Group':
+      ensure => present,
+      }
 }
