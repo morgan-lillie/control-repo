1787632275b00c420220e7e347872dc4178c0181
Morgan Lillie <61731997+morgan-lillie@users.noreply.github.com>
2020-11-17 15:50:56 +0000

    Update iis.pp

--- site-modules/profile/manifests/platform/baseline/windows/iis.pp
+++ site-modules/profile/manifests/platform/baseline/windows/iis.pp
@@ -7,11 +7,6 @@ dism { 'IIS-WebServer':
 
 
 
-$iis_features = ['Web-WebServer','Web-Scripting-Tools']
-
-iis_feature { $iis_features:
-  ensure => 'present',
-}
 
 # Delete the default website to prevent a port binding conflict.
 iis_site {'Default Web Site':
