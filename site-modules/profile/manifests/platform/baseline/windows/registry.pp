b34e9aef98c7e539b2255c718c78277ea2ba9f41
Morgan Lillie <61731997+morgan-lillie@users.noreply.github.com>
2020-11-17 14:40:31 +0000

    Update registry.pp

--- site-modules/profile/manifests/platform/baseline/windows/registry.pp
+++ site-modules/profile/manifests/platform/baseline/windows/registry.pp
@@ -1,12 +1,12 @@
 class profile::platform::baseline::windows::registry {
 
-registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}':
+registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
     ensure => present,
     data => '1',
     type => 'dword',
 }
 
-registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}':
+registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
     ensure => present,
     data => '1',
     type => 'dword',
