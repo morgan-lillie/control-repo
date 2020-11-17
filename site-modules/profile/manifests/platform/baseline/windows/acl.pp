490450d4fa4a9e9f856718614fddc77ef18662f3
Morgan Lillie <61731997+morgan-lillie@users.noreply.github.com>
2020-11-17 11:09:22 +0000

    Update acl.pp
    Changed permissions

--- site-modules/profile/manifests/platform/baseline/windows/acl.pp
+++ site-modules/profile/manifests/platform/baseline/windows/acl.pp
@@ -4,7 +4,7 @@ acl { 'c:/cont':
   permissions => [
    { identity => 'Administrator', rights => ['full'] },
    { identity => 'Cont User', rights => ['full'] },
-   { identity => 'Cont Group', rights => ['read'] },
+   { identity => 'Users', rights => ['read'] },
  ],
   owner => 'Cont User',
  
