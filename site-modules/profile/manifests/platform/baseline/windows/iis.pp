c0d20435e498b7bf1cf62bc0f6e6af2dc955019d
Morgan Lillie <61731997+morgan-lillie@users.noreply.github.com>
2020-11-17 15:04:53 +0000

    Update iis.pp

--- site-modules/profile/manifests/platform/baseline/windows/iis.pp
+++ site-modules/profile/manifests/platform/baseline/windows/iis.pp
@@ -1,8 +1,9 @@
 class profile::platform::baseline::windows::iis {
 
-dism { 'IIS-WebServer':
+$iis_features = ['Web-Server','Web-WebServer','Web-Asp-Net45','Web-ISAPI-Ext','Web-ISAPI-Filter','NET-Framework-45-ASPNET','WAS-NET-Environment','Web-Http-Redirect','Web-Filtering','Web-Mgmt-Console','Web-Mgmt-Tools']
+
+windowsfeature { $iis_features:
   ensure => present,
-  answer => 'C:\answer\iis.xml',
 }
 
 }
