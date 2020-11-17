2ea1a8a1bb9ea4d3b723653809a46a37254091d7
Morgan Lillie <61731997+morgan-lillie@users.noreply.github.com>
2020-11-17 16:19:36 +0000

    Update iis.pp

--- site-modules/profile/manifests/platform/baseline/windows/iis.pp
+++ site-modules/profile/manifests/platform/baseline/windows/iis.pp
@@ -32,4 +32,23 @@ file { 'minimal':
   path   => 'c:\\inetpub\\minimal',
 }
 
+iis_application_pool { 'minimal_site_app_pool':
+  ensure                  => 'present',
+  state                   => 'started',
+  managed_pipeline_mode   => 'Integrated',
+  managed_runtime_version => 'v4.0',
+} ->
+
+iis_site { 'minimal':
+  ensure          => 'started',
+  physicalpath    => 'c:\\inetpub\\minimal',
+  applicationpool => 'minimal_site_app_pool',
+  require         => File['minimal'],
+}
+
+file { 'minimal':
+  ensure => 'directory',
+  path   => 'c:\\inetpub\\minimal',
+}
+
 }
