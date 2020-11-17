49bc8f507a33b326e1c651543f54d937c8375ce5
Morgan Lillie <61731997+morgan-lillie@users.noreply.github.com>
2020-11-17 14:21:21 +0000

    Update registry.pp

--- site-modules/profile/manifests/platform/baseline/windows/registry.pp
+++ site-modules/profile/manifests/platform/baseline/windows/registry.pp
@@ -1,6 +1,6 @@
 class profile::platform::baseline::windows::registry {
 
-registry_value { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}':
+registry_value { 'HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Internet Settings\ZoneMap\IEHarden':
     ensure => present,
     data => '00000001',
     type => 'dword',
