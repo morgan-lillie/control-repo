7eeeb15c04db9a921e7d2d43a2c41a50320442b9
Morgan Lillie <61731997+morgan-lillie@users.noreply.github.com>
2020-11-17 16:41:22 +0000

    Update iis.pp

--- site-modules/profile/manifests/platform/baseline/windows/iis.pp
+++ site-modules/profile/manifests/platform/baseline/windows/iis.pp
@@ -1,54 +1,31 @@
 class profile::platform::baseline::windows::iis {
 
-dism { 'IIS-WebServer':
-  ensure => present,
-  answer => 'C:\answer\iis.xml',
-}
+  # Create Directories
 
-$iis_features = ['Web-WebServer','Web-Scripting-Tools']
+  $iis_features = ['Web-WebServer','Web-Scripting-Tools']
 
-iis_feature { $iis_features:
-  ensure => 'present',
-}
+  iis_feature { $iis_features:
+    ensure => 'present',
+  }
 
-## Delete the default website to prevent a port binding conflict.
-#iis_site {'Default Web Site':
-#  ensure  => absent,
-#  require => Iis_feature['Web-WebServer'],
-#}
+  # Delete the default website to prevent a port binding conflict.
+  iis_site {'Default Web Site':
+    ensure  => absent,
+    require => Iis_feature['Web-WebServer'],
+  }
 
-iis_site { 'minimal':
-  ensure          => 'started',
-  physicalpath    => 'c:\\inetpub\\minimal',
-  applicationpool => 'DefaultAppPool',
-  require         => [
-    File['minimal'],
-    Iis_site['Default Web Site']
-  ],
-}
+  iis_site { 'minimal':
+    ensure          => 'started',
+    physicalpath    => 'c:\\inetpub\\minimal',
+    applicationpool => 'DefaultAppPool',
+    require         => [
+      File['minimal'],
+      Iis_site['Default Web Site']
+    ],
+  }
 
-file { 'minimal':
-  ensure => 'directory',
-  path   => 'c:\\inetpub\\minimal',
-}
-
-iis_application_pool { 'minimal_site_app_pool':
-  ensure                  => 'present',
-  state                   => 'started',
-  managed_pipeline_mode   => 'Integrated',
-  managed_runtime_version => 'v4.0',
-} ->
-
-iis_site { 'minimal':
-  ensure          => 'started',
-  physicalpath    => 'c:\\inetpub\\minimal',
-  applicationpool => 'minimal_site_app_pool',
-  require         => File['minimal'],
-}
-
-file { 'minimal':
-  ensure => 'directory',
-  path   => 'c:\\inetpub\\minimal',
-}
-
+  file { 'minimal':
+    ensure => 'directory',
+    path   => 'c:/inetpub/minimal',
+  }
 }
