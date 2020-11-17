6714c0023931060b1b7fceb4a44f327dc129bed8
Morgan Lillie <61731997+morgan-lillie@users.noreply.github.com>
2020-11-17 11:34:00 +0000

    Update registry.pp

--- site-modules/profile/manifests/platform/baseline/windows/registry.pp
+++ site-modules/profile/manifests/platform/baseline/windows/registry.pp
@@ -3,5 +3,5 @@ registry_key { 'HKLM\System\CurrentControlSet\Services\Puppet':
 }
 
 registry_key { 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}':
-    ensure => absent
+    ensure => present
 }
