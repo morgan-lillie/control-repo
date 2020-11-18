c48a774eaf711421a70f4a91ccffe51a3365fc34
Morgan Lillie <61731997+morgan-lillie@users.noreply.github.com>
2020-11-18 10:12:04 +0000

    Update packages.pp

--- site-modules/profile/manifests/platform/baseline/windows/packages.pp
+++ site-modules/profile/manifests/platform/baseline/windows/packages.pp
@@ -1,14 +1,14 @@
-class profile::platform::baseline::windows::packages{
+#class profile::platform::baseline::windows::packages{
 
 
-class { 'archive':
-  seven_zip_name     => '7-Zip 9.20 (x64 edition)',
-  seven_zip_source   => 'C:/Windows/Temp/7z920-x64.msi',
-  seven_zip_provider => 'windows',
-}
+#class { 'archive':
+#  seven_zip_name     => '7-Zip 9.20 (x64 edition)',
+#  seven_zip_source   => 'C:/Windows/Temp/7z920-x64.msi',
+#  seven_zip_provider => 'windows',
+#
 
-include chocolatey
+#include chocolatey
 
-}
+#}
 
 
