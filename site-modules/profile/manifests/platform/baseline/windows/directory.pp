5118aeb33b73f2f23ba0a5b15ed9c45662d06f62
SE Demo User <morgan.lillie@puppet.com>
2020-12-11 11:22:04 +0000

    Updated using feedback

--- site-modules/profile/manifests/platform/baseline/windows/directory.pp
+++ site-modules/profile/manifests/platform/baseline/windows/directory.pp
@@ -1,7 +1,9 @@
+#ensures presence of cont directory and that it belongs to the Cont User
 class profile::platform::baseline::windows::directory {
 
   # TODO: Alignment
   # TODO: Work out how to ensure that the cont user is already there
+  require windows
   file { 'c:/cont':
     ensure => 'directory',
     owner  => 'Cont User',
--- site-modules/profile/manifests/platform/baseline/windows/windows.pp
+++ site-modules/profile/manifests/platform/baseline/windows/windows.pp
@@ -2,9 +2,9 @@ class profile::platform::baseline::windows::windows {
   
   # TODO: Indentation + alignment + hash rockets
   user { 'Cont User':
-    ensure => present,
+    ensure   => present,
     password => 'puppet',
-    groups => ['Administrators', 'Cont Group'],
+    groups   => ['Administrators', 'Cont Group'],
     }
    
   group { 'Cont Group':
@@ -12,7 +12,7 @@ class profile::platform::baseline::windows::windows {
       }
   #requires mod 'ayohrling-local_security_policy', '0.8.0'    
   local_security_policy { 'Log on as a service':
-  ensure => present,
+  ensure       => present,
   policy_value => 'Cont Group',
 }
 }
