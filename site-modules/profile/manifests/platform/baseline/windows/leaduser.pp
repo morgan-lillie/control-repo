000d0e5c53c3b3fad1d37b5178fc30253d82cd46
SE Demo User <morgan.lillie@puppet.com>
2021-02-23 13:16:09 +0000

    Corrected class name

--- site-modules/profile/manifests/platform/baseline/windows/leaduser.pp
+++ site-modules/profile/manifests/platform/baseline/windows/leaduser.pp
@@ -1,19 +1,19 @@
 #manages users, groups, and security policy
-class profile::platform::baseline::windows::leadusers {
+class profile::platform::baseline::windows::leaduser {
 
   # TODO: Indentation + alignment + hash rockets
   user { 'Lead User':
     ensure   => present,
     password => 'puppet',
     groups   => ['Administrators', 'Lead Group'],
-    }
+  }
 
   group { 'Lead Group':
-      ensure => present,
-      }
+    ensure => present,
+  }
   #requires mod 'ayohrling-local_security_policy', '0.8.0'    
   local_security_policy { 'Log on as a service':
-  ensure       => present,
-  policy_value => 'Lead Group',
-}
+    ensure       => present,
+    policy_value => 'Lead Group',
+  }
 }
