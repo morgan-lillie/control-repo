882753e5b092f3588504cc773f456550a6451c2c
SE Demo User <morgan.lillie@puppet.com>
2021-02-23 11:36:02 +0000

    corrected class name

--- site-modules/profile/manifests/platform/baseline.pp
+++ site-modules/profile/manifests/platform/baseline.pp
@@ -1,4 +1,4 @@
-class profile::baseline::windows{
+class profile::baseline{
   include profile::platform::baseline::windows::registry
   include profile::platform::baseline::windows::windows
   include profile::platform::baseline::windows::packages
