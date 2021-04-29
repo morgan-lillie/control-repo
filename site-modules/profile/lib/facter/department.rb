a509cc4c196867a848996b4539f2ad13206da119
SE Demo User <morgan.lillie@puppet.com>
2021-04-29 14:37:51 +0100

    Updat fact

--- site-modules/profile/lib/facter/department.rb
+++ site-modules/profile/lib/facter/department.rb
@@ -2,15 +2,15 @@ Facter.add(:department) do
     setcode do
         hostname = Facter.value('hostname')
         department = hostname[-1]
-        #if departmentid == 'n'
-        #    department = 'marketing'            
-        #elsif departmentid == 'e'
-        #    department = 'sales'
-        #elsif departmentid == 'i'
-        #    department = 'it'
-        #else
-        #    department = 'null'
-        #end
+        if departmentid == '1'
+            department = 'marketing'            
+        elsif departmentid == 'r'
+            department = 'sales'
+        elsif departmentid == 'i'
+            department = 'it'
+        else
+            department = 'null'
+        end
         department
 
     end
