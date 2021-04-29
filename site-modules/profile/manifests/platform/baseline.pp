ac5a32748bc82103259cdd972981b29d72e1a51f
SE Demo User <morgan.lillie@puppet.com>
2021-04-29 15:08:22 +0100

    Testing Hiera

--- site-modules/profile/manifests/platform/baseline.pp
+++ site-modules/profile/manifests/platform/baseline.pp
@@ -2,4 +2,5 @@ class profile::platform::baseline{
   include profile::platform::baseline::windows::registry
   include profile::platform::baseline::windows::leaduser
   include profile::platform::baseline::windows::packages
+  include profile::hiera_test
 }
